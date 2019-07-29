faca8590076e757ca67ff85d00b3a6a59741b961
tushx3 <31644180+tushx3@users.noreply.github.com>
2019-07-29 23:02:22 +0530

    Update tush.c

--- tush.c
+++ tush.c
@@ -4,17 +4,36 @@
 
 int main()
 {
-	int x;
-	char h[4], n[20], p[20], a[4] = "yes";
-	printf("want to register");
-	scanf("%s", &h);
-	x = strcmp(a,h);
-if (x == 0)
+	int p,l,m;
+	char check[4], name[20], pass[20], regc[9] = "register", y[4] = "yes", yc[4] , ycc[20], pc[20];
+	printf("want to register or login");
+	scanf("%s", &check);
+	p = strcmp(regc,check);
+if (p == 0)
 {
 	printf("enter name");
-	scanf("%s", &n);
+	scanf("%s", &name);
 	printf("enter password");
-	scanf("%s", &p);
+	scanf("%s", &pass);
+	printf("registration sucessfull want to login");
+	scanf("%s", &yc);
+	if (yc == 0) 
+	{
+            printf("enter username");
+            scanf("%s", &ycc);
+            printf("enter password");
+            scanf("%s", &pc);
+            l = strcmp(name , ycc);
+            m = strcmp(pass , pc);
+            if (l == 0 , m == 0)
+            {
+            	printf("logged in sucessfully");
+            }     
+            else
+            {
+            	printf("wrong password");
+            }
+	}
 
 }
 else 
